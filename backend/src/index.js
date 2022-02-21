c412f400cf2887178bdf1fca2733e2eb1cc0c54e
moonstar-x <christian.lopez99@outlook.com>
2022-02-21 10:57:00 -0500

    React webapp is now loaded in express server.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
+const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const logger = require('@greencoast/logger');
 
 const HTTP_PORT = process.env.PORT || 4000;
+const WEBAPP_DIR = process.env.WEBAPP_DIR;
+
+if (!fs.existsSync(WEBAPP_DIR)) {
+  throw new ReferenceError(`${WEBAPP_DIR} does not exist!`);
+}
 
 const app = express();
 app.use(cors());
 
 app.options('*', cors());
 
-app.get('/', (req, res) => {
-  res.status(200).send('It works!');
-});
+app.use(express.static(WEBAPP_DIR));
 
 app.all('*', (req, res) => {
   res.status(404).send('Not found!');
