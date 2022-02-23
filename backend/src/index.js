87125d3e3ec9cbbe288f1050be5ce2252b520f0f
moonstar-x <christian.lopez99@outlook.com>
2022-02-23 10:38:52 -0500

    Fixed SPA routing.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const logger = require('@greencoast/logger');
+const apiRouter = require('./api');
 const { logRequests, handleError } = require('./middleware');
 const { ResourceNotFoundError } = require('./errors');
 
@@ -13,6 +15,8 @@ if (!fs.existsSync(WEBAPP_DIR)) {
   throw new ReferenceError(`${WEBAPP_DIR} does not exist!`);
 }
 
+const WEBAPP_INDEX = path.join(WEBAPP_DIR, './index.html');
+
 const app = express();
 app.use(cors());
 app.use(logRequests);
@@ -20,6 +24,12 @@ app.use(logRequests);
 app.options('*', cors());
 
 app.use(express.static(WEBAPP_DIR));
+
+app.use('/api', apiRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(WEBAPP_INDEX);
+});
 
 app.all('*', (req, res) => {
   throw new ResourceNotFoundError('This route is not handled by the server.');
