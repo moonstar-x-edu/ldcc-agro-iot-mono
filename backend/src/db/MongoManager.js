c5e0bf9187b1f76e6ff0ca303b7dd12f38cf714a
moonstar-x <christian.lopez99@outlook.com>
2022-02-27 13:05:32 -0500

    Added event handlers to MongoManager.

--- backend/src/db/MongoManager.js
+++ backend/src/db/MongoManager.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const logger = require('@greencoast/logger');
 const UsersDatabase = require('./UsersDatabase');
 
 class MongoManager {
   constructor(uri) {
     this.mongo = this._createConnection(uri);
+    this.registerConnectionEvents();
 
     this.users = new UsersDatabase(this.mongo);
   }
@@ -13,6 +15,30 @@ class MongoManager {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
+  }
+
+  registerConnectionEvents() {
+    this.mongo.on('connecting', () => {
+      logger.info('(MONGO): Connecting to MongoDB...');
+    });
+
+    this.mongo.on('connected', () => {
+      logger.info('(MONGO): Connected to MongoDB.');
+    });
+
+    this.mongo.on('disconnected', () => {
+      logger.warn('(MONGO): Disconnected from MongoDB.');
+    });
+
+    this.mongo.on('reconnected', () => {
+      logger.info('(MONGO): Reconnected to MongoDB.');
+    });
+
+    this.mongo.on('error', (error) => {
+      logger.error('(MONGO): A connection error has occurred!', error);
+    });
+
+    return this;
   }
 }
 
