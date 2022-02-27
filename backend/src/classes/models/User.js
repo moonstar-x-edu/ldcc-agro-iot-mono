8422f7da01af9037c879464fe30282ca13841fa9
moonstar-x <christian.lopez99@outlook.com>
2022-02-27 11:03:57 -0500

    Created User model.

---
+++ backend/src/classes/models/User.js
@@ -0,0 +1,39 @@
+const Joi = require('joi');
+const { v4: uuid } = require('uuid');
+const SchemaValidator = require('../SchemaValidator');
+
+class User {
+  constructor(data = {}) {
+    this.data = data;
+  }
+
+  static from(obj, isNew = false) {
+    if (!isNew) {
+      return new User(SchemaValidator.validate(obj, User.UPDATE_SCHEMA));
+    }
+
+    const data = SchemaValidator.validate(obj, User.CREATE_SCHEMA);
+
+    data.id = uuid();
+
+    return new User(data);
+  }
+}
+
+User.CREATE_SCHEMA = Joi.object({
+  id: Joi.forbidden(),
+  name: Joi.string().trim().required(),
+  lastName: Joi.string().trim().required(),
+  profileURL: Joi.string().uri().allow(null).allow('').default(null),
+  devices: Joi.forbidden().default([])
+});
+
+User.UPDATE_SCHEMA = Joi.object({
+  id: Joi.forbidden(),
+  name: Joi.string().trim(),
+  lastName: Joi.string().trim(),
+  profileURL: Joi.string().uri().allow(null).allow(''),
+  devices: Joi.forbidden()
+});
+
+module.exports = User;
