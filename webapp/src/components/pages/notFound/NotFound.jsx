97709da34e5c755a266f161c28a4e87e745f796f
moonstar-x <christian.lopez99@outlook.com>
2022-03-04 14:07:27 -0500

    Added not found page.

--- webapp/src/components/pages/notFound/NotFound.jsx
+++ webapp/src/components/pages/notFound/NotFound.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import AppContext from '../../context/AppContext';
+import { updatePageTitle } from '../../../utils/page';
 
 const NotFound = () => {
+  const { setActive } = useContext(AppContext);
+
+  useEffect(() => {
+    setActive(null);
+    updatePageTitle('¡Página no encontrada!');
+  }, [setActive]);
+
   return (
-    <div>
-      404
-    </div>
+    <Container className="not-found-content">
+      <span role="img" aria-label="Person shrugging" className="error-emoji">
+        🤷🏻
+      </span>
+      <div className="error-title">
+        404
+      </div>
+      <div className="error-message">
+        La página que intentaste acceder no existe.
+      </div>
+    </Container>
   );
 };
 
