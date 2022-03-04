c8b85f72f773ed89ce369c3ef48dc3f49c255897
moonstar-x <christian.lopez99@outlook.com>
2022-03-04 13:57:37 -0500

    Styled navbar.

--- webapp/src/components/common/navbar/Navbar.jsx
+++ webapp/src/components/common/navbar/Navbar.jsx
@@ -1,27 +1,64 @@
-import React, { useContext } from 'react';
-import { Navbar as BSNavbar, Nav } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Navbar as BSNavbar, Nav, Image, Container, Offcanvas } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import AppContext from '../../context/AppContext';
+import UserContext from '../../context/UserContext';
 import { ROUTES, PAGES } from '../../../constants';
 
 const Navbar = () => {
   const { active } = useContext(AppContext);
+  const { user } = useContext(UserContext);
+
+  const [expand, setExpand] = useState(false);
+
+  const handleOpen = () => {
+    setExpand(true);
+  };
+
+  const handleClose = () => {
+    setExpand(false);
+  };
 
   return (
-    <BSNavbar collapseOnSelect expand="lg" variant="dark">
-      <BSNavbar.Brand as={Link} to={ROUTES.dashboard}>
-        <span>LOGO</span>
-      </BSNavbar.Brand>
-      <BSNavbar.Toggle aria-controls="responsive-sidebar" />
-      <BSNavbar.Collapse expand="lg" id="responsive-sidebar">
-        <Nav fill className="mr-auto">
+    <BSNavbar expand={false} variant="dark">
+      <Container className="mw-100 mx-2">
+        <BSNavbar.Toggle onClick={handleOpen} />
+        <Nav className="mx-4">
           <Nav.Item>
-            <Nav.Link as={Link} to={ROUTES.dashboard} active={active === PAGES.dashboard} eventKey={PAGES.dashboard}>
-              Dashboard
-            </Nav.Link>
+            {
+              user && user.profileURL ?
+                <Image className="user-icon" roundedCircle src={user.profileURL} /> :
+                <i className="fa-solid fa-user icon-white user-icon fa-xl" />
+            }
           </Nav.Item>
         </Nav>
-      </BSNavbar.Collapse>
+
+        <Offcanvas placement="start" show={expand} onHide={handleClose} scroll backdrop>
+          <button className="back-button" onClick={handleClose}>
+            <i className="fa-solid fa-arrow-right fa-xl" />
+          </button>
+          <Offcanvas.Header>
+            <Offcanvas.Title id="offcanvasNavbarLabel">
+              Sistema de Agricultura Inteligente
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  to={ROUTES.dashboard}
+                  active={active === PAGES.dashboard}
+                  eventKey={PAGES.dashboard}
+                  className="link"
+                >
+                  <i className="fa-solid fa-gauge " /> Dashboard
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </Container>
     </BSNavbar>
   );
 };
