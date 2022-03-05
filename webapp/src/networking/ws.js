65f06f6ff4e2dec08da468e7955b9a4f0b8f9f6a
moonstar-x <christian.lopez99@outlook.com>
2022-03-05 14:11:46 -0500

    Added wss to production URL.

--- webapp/src/networking/ws.js
+++ webapp/src/networking/ws.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 const WS_URL = process.env.NODE_ENV === 'development' ?
   'ws://localhost:4000' :
-  `ws://${window.location.host}`;
+  `wss://${window.location.host}`;
 
 export const createSocket = () => {
   return io(WS_URL);
